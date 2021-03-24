510115661ab3b33bef2cc97cef6744792592fe8e
Thomas Chabal <t.chabal@hotmail.fr>
2021-03-24 12:31:24 +0100

    Cleaned notebook

--- online_learning/utils.py
+++ online_learning/utils.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt
 
 
 def add_text_on_image(img, texts=[], scale=1, bottom_left=(10, 200), color=(255, 0, 0), line_type=2):
@@ -18,3 +19,11 @@ def add_text_on_image(img, texts=[], scale=1, bottom_left=(10, 200), color=(255,
     if ratio > 1:
         img = cv2.resize(img, (h, w))
     return img
+
+
+def show_imgs(imgs, titles):
+    _, axes = plt.subplots(ncols=len(imgs), figsize=(15, 15))
+    for idx, img in enumerate(imgs):
+        axes[idx].imshow(img)
+        axes[idx].set_title(titles[idx])
+    plt.show()
